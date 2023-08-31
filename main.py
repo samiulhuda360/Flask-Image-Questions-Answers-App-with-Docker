de961b28b1954dc18dd862920d934cb0ef4ac61b
samiulhuda360 <samidurbar@gmail.com>
2023-09-01 04:33:02 +0600

    Resolved merge conflicts

---
+++ main.py
@@ -0,0 +1,22 @@
+from model import model_pipeline
+from typing import Union
+
+from fastapi import FastAPI, UploadFile
+import io
+from PIL import Image
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"hello": "World"}
+
+@app.post("/ask")
+def ask(text: str, image: UploadFile):
+    content = image.file.read()
+    
+    image = Image.open(io.BytesIO(content))
+    # image = Image.open(image.file)
+    
+    result = model_pipeline(text, image)
+    return {"answer": result}
